da8d2ee43d14801546b57becff65745fd06aa1f2
amalit2001 <amalit@afk.no>
2026-01-20 06:09:14 +0100

    Lagt til skjema for registrering av humør og energi (+ kort refleksjon)

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        mood = request.form["mood"]
+        energy = request.form["energy"]
+        note = request.form["note"]
+
+        print("Humør:", mood)
+        print("Energi:", energy)
+        print("Notat:", note)
+
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-

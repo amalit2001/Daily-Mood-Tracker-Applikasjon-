ad3e1d98764641cef8f304828778d8a82fca0694
amalit2001 <amalit@afk.no>
2026-01-20 05:38:20 +0100

    La til grunnleggende HTML template og render med Flask

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Daily Mood Tracker is running 🌱"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
